5e8c83e12af3731474f1c7d0f406e5789865356f
ncgardiner <ncgardiner@NN123-HA-21.HS.sd203.org>
2016-03-08 14:12:21 -0600

    onglemongle

--- PalindromeTester/PalindromeTester.java
+++ PalindromeTester/PalindromeTester.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
  * Demonstrates the use of nested while loops.
  * 
- * @author Lewis/Loftus/Cocking
+ * @author Lewis/Loftus/Cock
  */
 public class PalindromeTester
 {
@@ -21,26 +21,28 @@ public class PalindromeTester
       {
          System.out.println ("Enter a potential palindrome:");
          str = s.nextLine();
-
-         left = 0;
-         right = str.length() - 1;
-
-         while (str.charAt(left) == str.charAt(right) && left < right)
+         if (isPalindrome(str)==true)
          {
-            left++;
-            right--;
+             System.out.println ("That string is a palindrome.");
          }
-
-         System.out.println();
-
-         if (left < right)
-            System.out.println ("That string is NOT a palindrome.");
          else
-            System.out.println ("That string IS a palindrome.");
-
+         {
+             System.out.println ("That string is NOT a palindrome.");
+         }
+         
          System.out.println();
          System.out.print ("Test another palindrome (y/n)? ");
          another = s.nextLine();
       }
+   }
+   
+   public static boolean isPalindrome(String str)
+   {
+       if (str.length()<=1)
+       {
+           return true;
+       }
+       String substr = str.substring(1,str.length()-1);
+       return ((str.charAt(0)==str.charAt(str.length()-1))&& isPalindrome(substr));
    }
 }
